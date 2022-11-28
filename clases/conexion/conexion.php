64f08a50f34e6eef76912f19e4bfd3e96e6af0f5
jusepthm <jusepth.dev@gmail.com>
2022-11-27 19:37:13 -0500

    creando funcion constructor de la conexion

--- clases/conexion/conexion.php
+++ clases/conexion/conexion.php
@@ -6,8 +6,23 @@ class conexion {
     private $password;
     private $database;
     private $port;
+    private $conexion;
 
-
+function __construct(){
+    $listadatos = $this->datosConexion();
+    foreach($listadatos as $key => $vaule){
+        $this->server = $value['server'];
+        $this->user = $value['user'];
+        $this->password = $value['password'];
+        $this->database = $value['database'];
+        $this->port = $value['port'];
+    }
+    $this->conexion = new mysql($this->server,$this->user,$this->password,$this->database,$this->port);
+    if($this->conexion->connect_errno){
+        echo "Conexion fallida";
+        die();
+    }
+}
 
 private function datosConexion(){
     $direccion = dirname(__FILE__);
