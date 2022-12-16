170eede6cc75b1ef6520bab2bbafe078b1281cbb
jusepthm <jusepth.dev@gmail.com>
2022-12-16 14:34:32 -0500

    creando funcion listar pacientes y obtener paciente

--- clases/pacientes.class.php
+++ clases/pacientes.class.php
@@ -1,5 +1,29 @@
 <?php
+require_once 'conexion/conexion.php';
+require_once 'respuestas.class.php';
 
+class pacientes extends conexion {
+
+    private $table = "pacientes";
+
+    public function listaPacientes($pagina = 1){
+        $inicio = 0;
+        $cantidad = 100;
+        if($pagina > 1){
+            $inicio = ($cantidad*($pagina - 1)) +1;
+            $cantidad = $cantidad * $pagina;
+        }
+
+        $query = "SELECT PacienteId,Nombre,DNI,Telefono,Correo FROM ".$this->table." limit $inicio,$cantidad";
+        $datos = parent::obtenerDatos($query);
+        return ($datos);
+
+    }
+
+    public function obtenerPaciente($id){
+        $query = "SELECT * FROM ".$this->table." WHERE PacienteId = '$id'";
+    }
+}
 
 
 ?>
--- pacientes.php
+++ pacientes.php
@@ -3,11 +3,17 @@ require_once 'clases/respuestas.class.php';
 require_once 'clases/pacientes.class.php';
 
 $_respuestas = new respuestas;
-//$_pacientes = new pacientes;
+$_pacientes = new pacientes;
 
 
  if($_SERVER['REQUEST_METHOD'] == "GET"){
-    echo "Hola GET";
+
+    if(isset($_GET["page"])){
+        $pagina = $_GET["page"];
+        $listaPacientes = $_pacientes->listaPacientes($pagina);
+        echo json_encode($listaPacientes);
+    }
+    
  }else if($_SERVER['REQUEST_METHOD'] == "POST"){
     echo "Hola POST";
  }else if($_SERVER['REQUEST_METHOD'] == "PUT"){
